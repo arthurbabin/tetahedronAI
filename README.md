6240ffe69fcb4b1d21798db28ae47734ee672f51
Arthur <Arthur@fedora.home>
2023-01-10 14:12:51 +0100

    add docstrings and class diagram

--- README.md
+++ README.md
@@ -5,6 +5,69 @@ AI playing Solo Tetahedron Game in Python
 
 On a board of triangular tiles, the goal is to "roll" a pyramid with 4 faces (called a tetahedron). The pyramid moves by tilting from one face to another, always perfectly landing on a triangle.
 
+# Class Diagram
+
+```mermaid
+classDiagram
+    class Game {
+        int LINE_NB
+        int shapeWidth
+        int shapeHeight
+        int borderSize
+        pygame.Color BG_COLOR
+        List~pygame.Color~ COLORS
+        int screenWidth
+        int screenHeight
+        bool running
+        pygame.Surface screen
+        void updateMode(string mode)
+        void drawShape(int i, int j)
+        void drawPiece(int i,int j)
+        void render()
+        void verifyEvents()
+        void displayMessage(string msg)
+        bool wantsToContinue()
+    }
+
+	class AbstractGrid {
+			int width, height
+			Matrix~int~ grid
+			Matrix~bool~ isInValueList(List~int~ valueList)
+			selectRandomCasesWithSolution(int wantedValue)
+			void changeValue(int i, int j, int value)
+			bool isCaseUp(int i, int j)
+			bool isValidMove(int i, int j,string direction, int outOfBoardValue)
+            (int,int) getDestinationPosition(int i,int j, string direction)
+		}
+    class TetraGrid {
+        int ccIndex(int i, int j)
+    }
+	class Board {
+			List~pygame.Color~ colors
+			AbstractGrid grid
+			AbstractColoredPiece playerPiece
+			int,int playerPosition
+			int numberOfColors
+			int outOfBoardCaseValue
+			int defaultCaseValue
+			int underPlayerPieceCaseValue
+			void randomizeColorCases()
+            bool isValidMove(string direction)
+            void move(string direction)
+			bool checkWin()
+		}
+	class AbstractColoredPiece {
+			pygame.Color defaultColor
+			Dict~string:pygame.Color~ faces
+			void move(string direction, int colorOfDestination)
+			bool isFullyColored()
+		}
+	class ColoredTetahedron {
+	}
+	ColoredTetahedron --|> AbstractColoredPiece
+    TetraGrid --|> AbstractGrid
+```
+
 # The Board
 <br>
 <img src="https://github.com/arthurbabin/tetahedronAI/blob/main/image_2023-01-04_195906531.png?raw=true"/>
--- main.py
+++ main.py
@@ -1,128 +1,193 @@
 import pygame
-from board import Board 
+from board import Board
+
 
 class Game:
-    LINE_NB = 4 # Number of lines on the board (must be even)
-    shapeWidth = 90 # size of the shapes
-    shapeHeight= 90 # size of the shapes
-    borderSize= 3
-    BG_COLOR = (36,36,36) # Background color
-    COLORS = ["white","black","red","green","blue"]
+    """
+    Class representing the Tetrahedron/Octahedron/... Game
+    It has methods to display everything with pygame
+    """
 
-    def __init__(self,colors,mode="tetra"):
+    LINE_NB = 8  # Number of lines on the board (must be even)
+    shapeWidth = 90  # size of the shapes
+    shapeHeight = 90  # size of the shapes
+    borderSize = 3
+    BG_COLOR = (36, 36, 36)  # Background color
+
+    def __init__(self, colors, mode="tetra"):
+        """
+        Initialization of the class
+
+        :colors: the list of pygame colors used for the game (the first one
+        being the default color)
+        :mode: "tetra" (or "octa" but not implemented yet) corresponding to the
+        shape of the piece the player wants to play with
+        """
+        self.COLORS = colors
         pygame.init()
         pygame.display.set_caption("Tetrahedron Game")
         self.updateMode(mode=mode)
-        self.isWin = False
 
-    def updateMode(self,mode="tetra"):
-        self.board = Board(self.COLORS,self.LINE_NB,mode=mode)
-        #if mode=="tetra" or True: #TODO implement mode octa
-        self.screenWidth = self.shapeWidth+(self.shapeWidth/2+self.borderSize)*(self.board.grid.grid.shape[1]-1)
-        self.screenHeight = (self.shapeHeight+self.borderSize)*self.board.grid.grid.shape[0]-self.borderSize
-        self.screen = pygame.display.set_mode((self.screenWidth,self.screenHeight))
-        self.running = True #Execute the program until the user closes the window
+    def updateMode(self, mode="tetra"):
+        """
+        Change the mode of the game
 
-    def drawShape(self,i,j):
-        color = self.COLORS[int(self.board.grid.grid[i,j])]
-        #if self.mode=="tetra" or True: #TODO implement mode octa
-        centerX = 0.5*self.shapeWidth+j/2*self.shapeWidth+j*self.borderSize
-        centerY = 0.5*self.shapeHeight+i*self.shapeHeight+i*self.borderSize
-        if self.board.grid.isCaseUp(i,j):
+        :mode: "tetra" (or "octa" but not implemented yet) corresponding to the
+        shape of the piece the player wants to play with
+        :return: nothing
+        """
+        self.board = Board(self.COLORS, self.LINE_NB, mode=mode)
+        # if mode=="tetra" or True: #TODO implement mode octa
+        self.screenWidth = self.shapeWidth + (self.shapeWidth / 2 + self.borderSize) * (
+            self.board.grid.grid.shape[1] - 1
+        )
+        self.screenHeight = (
+            self.shapeHeight + self.borderSize
+        ) * self.board.grid.grid.shape[0] - self.borderSize
+        self.screen = pygame.display.set_mode((self.screenWidth, self.screenHeight))
+        self.running = True  # Execute the program until the user closes the window
+
+    def drawShape(self, i, j):
+        """
+        Draw the shape (triangle if the mode is tetra etc) with
+        the selected coordinates
+
+        :i: line coordinate in the grid
+        :j: column coordinate in the grid
+        :return: nothing
+        """
+        color = self.COLORS[int(self.board.grid.grid[i, j])]
+        # if self.mode=="tetra" or True: #TODO implement mode octa
+        centerX = 0.5 * self.shapeWidth + j / 2 * self.shapeWidth + j * self.borderSize
+        centerY = 0.5 * self.shapeHeight + i * self.shapeHeight + i * self.borderSize
+        if self.board.grid.isCaseUp(i, j):
             points = [
-                    (centerX,centerY-0.5*self.shapeHeight),
-                    (centerX+0.5*self.shapeHeight,centerY+0.5*self.shapeHeight),
-                    (centerX-0.5*self.shapeHeight,centerY+0.5*self.shapeHeight)
-                    ]
+                (centerX, centerY - 0.5 * self.shapeHeight),
+                (centerX + 0.5 * self.shapeHeight, centerY + 0.5 * self.shapeHeight),
+                (centerX - 0.5 * self.shapeHeight, centerY + 0.5 * self.shapeHeight),
+            ]
         else:
             points = [
-                    (centerX,centerY+0.5*self.shapeHeight),
-                    (centerX+0.5*self.shapeHeight,centerY-0.5*self.shapeHeight),
-                    (centerX-0.5*self.shapeHeight,centerY-0.5*self.shapeHeight)
-                    ]
-        pygame.draw.polygon(self.screen,color,points)
+                (centerX, centerY + 0.5 * self.shapeHeight),
+                (centerX + 0.5 * self.shapeHeight, centerY - 0.5 * self.shapeHeight),
+                (centerX - 0.5 * self.shapeHeight, centerY - 0.5 * self.shapeHeight),
+            ]
+        pygame.draw.polygon(self.screen, color, points)
 
-    def drawPiece(self,i,j):
-        color = self.COLORS[int(self.board.grid.grid[i,j])]
-        #if self.mode=="tetra" or True: #TODO implement mode octa
-        centerX = 0.5*self.shapeWidth+j/2*self.shapeWidth+j*self.borderSize
-        centerY = 0.5*self.shapeHeight+i*self.shapeHeight+i*self.borderSize
-        center = centerX,centerY
-        lowerLeft = centerX-0.5*self.shapeHeight,centerY+0.5*self.shapeHeight
-        upperLeft = centerX-0.5*self.shapeHeight,centerY-0.5*self.shapeHeight
-        lowerRight= centerX+0.5*self.shapeHeight,centerY+0.5*self.shapeHeight
-        upperRight= centerX+0.5*self.shapeHeight,centerY-0.5*self.shapeHeight
-        lowerMiddle= centerX,centerY+0.5*self.shapeHeight
-        upperMiddle= centerX,centerY-0.5*self.shapeHeight
-        if self.board.grid.isCaseUp(i,j):
-            leftFace = [upperMiddle,center,lowerLeft]
-            rightFace = [upperMiddle,center,lowerRight]
-            otherFace = [lowerLeft,center,lowerRight]
+    def drawPiece(self, i, j):
+        """
+        Draw the player's piece (tetrahedron if the mode is tetra etc) with the
+        selected coordinates)
+
+        :i: line coordinate in the grid
+        :j: column coordinate in the grid
+        :return: nothing
+        """
+        color = self.COLORS[int(self.board.grid.grid[i, j])]
+        # if self.mode=="tetra" or True: #TODO implement mode octa
+        centerX = 0.5 * self.shapeWidth + j / 2 * self.shapeWidth + j * self.borderSize
+        centerY = 0.5 * self.shapeHeight + i * self.shapeHeight + i * self.borderSize
+        center = centerX, centerY
+        lowerLeft = centerX - 0.5 * self.shapeHeight, centerY + 0.5 * self.shapeHeight
+        upperLeft = centerX - 0.5 * self.shapeHeight, centerY - 0.5 * self.shapeHeight
+        lowerRight = centerX + 0.5 * self.shapeHeight, centerY + 0.5 * self.shapeHeight
+        upperRight = centerX + 0.5 * self.shapeHeight, centerY - 0.5 * self.shapeHeight
+        lowerMiddle = centerX, centerY + 0.5 * self.shapeHeight
+        upperMiddle = centerX, centerY - 0.5 * self.shapeHeight
+        if self.board.grid.isCaseUp(i, j):
+            leftFace = [upperMiddle, center, lowerLeft]
+            rightFace = [upperMiddle, center, lowerRight]
+            otherFace = [lowerLeft, center, lowerRight]
         else:
-            leftFace = [upperLeft,center,lowerMiddle]
-            rightFace = [upperRight,center,lowerMiddle]
-            otherFace = [upperLeft,center,upperRight]
+            leftFace = [upperLeft, center, lowerMiddle]
+            rightFace = [upperRight, center, lowerMiddle]
+            otherFace = [upperLeft, center, upperRight]
         leftColor = self.COLORS[int(self.board.playerPiece.faces["left"])]
         rightColor = self.COLORS[int(self.board.playerPiece.faces["right"])]
         otherColor = self.COLORS[int(self.board.playerPiece.faces["other"])]
-        pygame.draw.polygon(self.screen,leftColor,leftFace)
-        pygame.draw.polygon(self.screen,rightColor,rightFace)
-        pygame.draw.polygon(self.screen,otherColor,otherFace)
-        if self.board.grid.isCaseUp(i,j):
-            pygame.draw.polygon(self.screen,"black",[lowerLeft,center],width=3)
-            pygame.draw.polygon(self.screen,"black",[lowerRight,center],width=3)
-            pygame.draw.polygon(self.screen,"black",[upperMiddle,center],width=3)
+        pygame.draw.polygon(self.screen, leftColor, leftFace)
+        pygame.draw.polygon(self.screen, rightColor, rightFace)
+        pygame.draw.polygon(self.screen, otherColor, otherFace)
+        if self.board.grid.isCaseUp(i, j):
+            pygame.draw.polygon(self.screen, "black", [lowerLeft, center], width=3)
+            pygame.draw.polygon(self.screen, "black", [lowerRight, center], width=3)
+            pygame.draw.polygon(self.screen, "black", [upperMiddle, center], width=3)
         else:
-            pygame.draw.polygon(self.screen,"black",[upperLeft,center],width=3)
-            pygame.draw.polygon(self.screen,"black",[upperRight,center],width=3)
-            pygame.draw.polygon(self.screen,"black",[lowerMiddle,center],width=3)
+            pygame.draw.polygon(self.screen, "black", [upperLeft, center], width=3)
+            pygame.draw.polygon(self.screen, "black", [upperRight, center], width=3)
+            pygame.draw.polygon(self.screen, "black", [lowerMiddle, center], width=3)
 
     def render(self):
+        """
+        Display the board with the player's piece at current state
+        """
         self.screen.fill(self.BG_COLOR)
-        playerX,playerY = self.board.playerPosition
-        for i,j in self.board.grid.validIndices:
-            self.drawShape(i,j)
-        self.drawPiece(playerX,playerY)
+        playerX, playerY = self.board.playerPosition
+        for i, j in self.board.grid.validIndices:
+            self.drawShape(i, j)
+        self.drawPiece(playerX, playerY)
         pygame.display.update()
 
     def verifyEvents(self):
+        """
+        Looks for Pygame events such as key inputs in order to update the board
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and game.board.isValidMove(direction="left"):
+                if event.key == pygame.K_LEFT and game.board.isValidMove(
+                    direction="left"
+                ):
                     self.board.move(direction="left")
-                elif event.key == pygame.K_RIGHT and game.board.isValidMove(direction="right"):
+                elif event.key == pygame.K_RIGHT and game.board.isValidMove(
+                    direction="right"
+                ):
                     self.board.move(direction="right")
-                elif event.key == pygame.K_UP and game.board.isValidMove(direction="up"):
+                elif event.key == pygame.K_UP and game.board.isValidMove(
+                    direction="up"
+                ):
                     self.board.move(direction="up")
-                elif event.key == pygame.K_DOWN and game.board.isValidMove(direction="down"):
+                elif event.key == pygame.K_DOWN and game.board.isValidMove(
+                    direction="down"
+                ):
                     self.board.move(direction="down")
 
-    def displayMessage(self,msg):
+    def displayMessage(self, msg):
+        """
+        Display the message to the player
+
+        :msg: string representing the message
+        :return: nothing
+        """
         #  font_style = pygame.font.SysFont(None,100)
         #  renderedMsg = font_style.render(msg,True,"red")
         #  self.screen.blit(renderedMsg,[self.screenWidth/2,self.screenHeight/2])
         print(msg)
 
     def wantsToContinue(self):
+        """
+        Ask the player weither or not he wants to continue playing
+
+        :return: boolean corresponding to the response
+        """
         prompt = "Do you want to play again ? (y/n) "
         while True:
             try:
-               return {"y":True,"n":False}[input(prompt).lower()]
+                return {"y": True, "n": False}[input(prompt).lower()]
             except KeyError:
-               print("Invalid input please enter True or False!")
+                print("Invalid input please enter True or False!")
 
 
 if __name__ == "__main__":
     # !!! Default case color value first
-    colors = ["white","black","red","green","blue"]
+    colors = ["white", "black", "red", "green", "blue"]
     wantsToContinue = True
     while wantsToContinue:
-        #Create Game
-        game = Game(colors,mode="tetra")
+        # Create Game
+        game = Game(colors, mode="tetra")
 
-        #Game loop
+        # Game loop
         while game.running:
             # Verify Pygame events
             game.verifyEvents()
@@ -142,5 +207,5 @@ if __name__ == "__main__":
         # Quit Pygame
         pygame.quit()
 
-        #Ask the user to play again 
+        # Ask the user to play again
         wantsToContinue = game.wantsToContinue()
